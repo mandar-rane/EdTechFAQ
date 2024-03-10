6e93d677fdc15c1928e2067af94796d7b01227d2
mandar-rane <mandar123rane@gmail.com>
2024-03-10 18:28:16 +0530

    as

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import streamlit as st
 from langchain_helper import create_vector_db
 from langchain_helper import get_qa_chain
 
-st.title("Course QA 🌱")
+st.title("Educational Course QnA 🌱")
+url = "https://github.com/mandar-rane"
+st.text("- Mandar Rane")
+st.write("GitHub: (%s)" % url)
 
-btn = st.button("Create knowledgebase")
+# btn = st.button("Create knowledgebase")
+# if btn:
+#     pass
+st.text("Example questions: \nQ. What are the prerequisites of this course?\nQ. What is the duration of this course?\nQ. I don't have a laptop, can I take this course?")
 
-if btn:
-    pass
 
-question = st.text_input("Question: ")
+question = st.text_input("Question (Press Enter for answer): ")
 
 if question:
     chain = get_qa_chain()
